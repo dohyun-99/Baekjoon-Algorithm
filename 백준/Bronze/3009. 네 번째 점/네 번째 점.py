7dfed611cfd118c2ce7ed8592a43410a8a50fec6
Dohyun <104931224+dohyun-99@users.noreply.github.com>
2023-08-01 16:03:44 +0900

    [Bronze III] Title: 네 번째 점, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,19 @@
+x, y = [], []
+
+for i in range(3):
+    a,b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+
+x.sort()
+y.sort()
+
+if x[1] == x[0]:
+    print(x[2], end=" ")
+else:
+    print(x[0], end=" ")
+
+if y[1] == y[0]:
+    print(y[2])
+else:
+    print(y[0])
