e6211968b7cd8d103db25993cb894d26e7c51d11
Dohyun <104931224+dohyun-99@users.noreply.github.com>
2023-08-18 09:56:44 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.98 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,14 @@
+def solution(s):
+    alpha_dict = {}
+    for x in range(len(s)):
+        if s[x] in alpha_dict.keys():
+            alpha_dict[s[x]].append(x)
+        else:
+            alpha_dict[s[x]] = [x]
+    print(alpha_dict)
+    answer = [-1] * len(s)
+    for y in alpha_dict:
+        if len(alpha_dict[y]) > 1:
+            for z in range(1, len(alpha_dict[y])):
+                answer[alpha_dict[y][z]] = alpha_dict[y][z] - alpha_dict[y][z-1]
+    return answer
